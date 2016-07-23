4d453c778745d016eb84cae1e94fbc5326411ad4
Markus Elfring <elfring@users.sourceforge.net>
2016-07-23 17:04:00 +0200

    PM-wakeup: Delete unnecessary checks before three function calls
    The following functions test whether their argument is NULL
    and then return immediately.
     * dev_pm_arm_wake_irq
     * dev_pm_disarm_wake_irq
     * wakeup_source_unregister
    
    Thus the test around the calls is not needed.
    
    This issue was detected by using the Coccinelle software.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    [ rjw: Minor whitespace adjustments ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -386,8 +386,7 @@ void device_wakeup_arm_wake_irqs(void)
 
 	srcuidx = srcu_read_lock(&wakeup_srcu);
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
-		if (ws->wakeirq)
-			dev_pm_arm_wake_irq(ws->wakeirq);
+		dev_pm_arm_wake_irq(ws->wakeirq);
 	}
 	srcu_read_unlock(&wakeup_srcu, srcuidx);
 }
@@ -404,8 +403,7 @@ void device_wakeup_disarm_wake_irqs(void)
 
 	srcuidx = srcu_read_lock(&wakeup_srcu);
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
-		if (ws->wakeirq)
-			dev_pm_disarm_wake_irq(ws->wakeirq);
+		dev_pm_disarm_wake_irq(ws->wakeirq);
 	}
 	srcu_read_unlock(&wakeup_srcu, srcuidx);
 }
@@ -442,9 +440,7 @@ int device_wakeup_disable(struct device *dev)
 		return -EINVAL;
 
 	ws = device_wakeup_detach(dev);
-	if (ws)
-		wakeup_source_unregister(ws);
-
+	wakeup_source_unregister(ws);
 	return 0;
 }
 EXPORT_SYMBOL_GPL(device_wakeup_disable);
