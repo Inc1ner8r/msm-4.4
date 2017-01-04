7c2c78577cf80f91fcb6df47eabff935653fdb15
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-01-04 16:51:53 +0100

    soc: qcom: stop spam when IPC_LOGGING is disabled
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>
    Signed-off-by: incinerator17 <punarvpawade17@gmail.com>

--- drivers/devfreq/devfreq_spdm.c
+++ drivers/devfreq/devfreq_spdm.c
@@ -366,6 +366,7 @@ static int probe(struct platform_device *pdev)
 	}
 
 #ifdef CONFIG_DEBUG_FS
+#ifdef CONFIG_IPC_LOGGING
 	spdm_init_debugfs(&pdev->dev);
 #endif
 	spdm_ipc_log_ctxt = ipc_log_context_create(SPDM_IPC_LOG_PAGES,
@@ -375,7 +376,7 @@ static int probe(struct platform_device *pdev)
 		pr_err("%s: Failed to create IPC log context\n", __func__);
 		spdm_ipc_log_ctxt = NULL;
 	}
-
+#endif
 
 	return 0;
 
