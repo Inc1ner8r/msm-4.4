50b201981ac653f5c35e1c3a7fe463cf969fe0d4
ajaivasudeve <ajaivasudeve@gmail.com>
2020-03-14 14:16:08 +0530

    kernel/cpu-boost: disable energy awareness for app launches
    ref: https://github.com/idkwhoiam322/weeb_kernel_oneplus_msm8998/commit/8a784135a2a025befd6614fd607278124cb55f77
    
    Signed-off-by: ajaivasudeve <ajaivasudeve@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -81,6 +81,8 @@
 #include <linux/cpufreq.h>
 #include <linux/simple_lmk.h>
 #include <linux/cpu_boost.h>
+#include <linux/devfreq_boost.h>
+#include <linux/sched/sysctl.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1801,6 +1803,7 @@ long _do_fork(unsigned long clone_flags,
 	/* Boost CPU to the max for 1500 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid) &&
 		time_before(jiffies, last_input_time + msecs_to_jiffies(500))) {
+                sysctl_sched_energy_aware = 0;
 		input_boost_max_kick(1500);
 	}
 
